248f80b2023e8a5779f4d61046394ae3115f543b
Robert M Ochshorn <mail@RMOZONE.COM>
2025-12-02 15:18:28 -0800

    use PORT env

--- serve-bucket/serve_waitress.py
+++ serve-bucket/serve_waitress.py
@@ -2,7 +2,7 @@ from waitress import serve
 from serve import app
 import os
 
-PORT=int(os.environ.get('SERVER_PORT', 8080))
+PORT=int(os.environ.get('PORT', 8080))
 
 print(f"SERVING ON PORT {PORT}")
 serve(app, host='0.0.0.0', port=PORT)
